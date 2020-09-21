d80a0efa34ddd37406d05e53487b1deaf8a03e84
nellika <nynelli@yahoo.de>
2020-09-21 12:43:18 +0200

    modify strange pooling function...

--- FLOAT/pool.c
+++ FLOAT/pool.c
@@ -24,14 +24,14 @@ void Pool1_24x24x20_2x2x20_2_0(	float 	input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1
 
     for (i = 0; i < CONV1_NBOUTPUT; i++){
       out_h=0;
-      out_w=0;
       for (h = 0; h < CONV1_HEIGHT; h+=2){
+        out_w=0;
         for (w = 0; w < CONV1_WIDTH; w+=2){
             maxPool=maxPooling((float []){input[i][h][w],input[i][h+1][w],input[i][h][w+1],input[i][h+1][w+1]});
             output[i][out_h][out_w]=maxPool;
             out_w+=1;
         }
-        out_h+1;
+        out_h+=1;
       }
     }
 }
@@ -44,14 +44,14 @@ void Pool2_8x8x40_2x2x40_2_0(	float 	input[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_W
 
     for (j = 0; j < CONV2_NBOUTPUT; j++){
       out_h=0;
-      out_w=0;
       for (h = 0; h < CONV2_HEIGHT; h+=2){
+        out_w=0;
         for (w = 0; w < CONV2_WIDTH; w+=2){
             maxPool=maxPooling((float []){input[j][h][w],input[j][h+1][w],input[j][h][w+1],input[j][h+1][w+1]});
             output[j][out_h][out_w]=maxPool;
             out_w+=1;
         }
-        out_h+1;
+        out_h+=1;
       }
     }
 }
