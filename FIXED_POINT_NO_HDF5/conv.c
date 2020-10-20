50fbe8daa031dda6119bae5eddf8fc703b49eaa1
nellika <nynelli@yahoo.de>
2020-10-20 15:36:55 +0200

    remove hdf5 reads

---
+++ FIXED_POINT_NO_HDF5/conv.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lenet_cnn_float.h"
+
+short sumProduct( short imgPart[CONV1_DIM][CONV1_DIM], short filter[CONV1_DIM][CONV1_DIM]){
+  unsigned short y,x;
+  int conv_result=0;
+
+  for(y = 0; y < CONV1_DIM; y++){
+      for(x = 0; x < CONV1_DIM; x++){
+        conv_result+=imgPart[y][x]*filter[y][x];
+      }
+  }
+  return conv_result >> FIXED_POINT;
+}
+
+void Conv1_28x28x1_5x5x20_1_0(  unsigned char input[IMG_DEPTH][IMG_HEIGHT][IMG_WIDTH], 	                // IN [1][28][28]
+				                short kernel[CONV1_NBOUTPUT][IMG_DEPTH][CONV1_DIM][CONV1_DIM], 	// IN [20][1][5][5]
+				                short bias[CONV1_NBOUTPUT],						                // IN [20]
+				                short output[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH])        // OUT [20][24][24]
+{
+  unsigned short o,h,w,x,y;
+  short imgPart[CONV1_DIM][CONV1_DIM];
+  short conv_px;
+  
+  for(o = 0; o < CONV1_NBOUTPUT; o++){
+    for(h = 0; h < CONV1_HEIGHT; h++){
+      for(w = 0; w < CONV1_WIDTH; w++){
+        //prepare 5X5 imgPart for the convolution
+        for(y = 0; y < CONV1_DIM; y++){
+          for(x = 0; x < CONV1_DIM; x++){
+            imgPart[y][x]=input[0][h+y][w+x];
+          }
+        }
+        conv_px=sumProduct(imgPart,kernel[o][0]);
+        
+        //neuron activation
+        //<=-100 works with better accuracy, but thats not the point here...
+        if(conv_px+bias[o]<=0){
+          output[o][h][w]=0;
+        }else{
+          output[o][h][w]=conv_px + bias[o];
+        }
+        
+      }
+    }
+  }
+  
+}
+
+void Conv2_12x12x20_5x5x40_1_0( short input[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH], 	            // IN
+				                short kernel[CONV2_NBOUTPUT][POOL1_NBOUTPUT][CONV2_DIM][CONV2_DIM], 	// IN
+				                short bias[CONV2_NBOUTPUT], 						                    // IN
+				                short output[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WIDTH]) 		        // OUT
+{
+  unsigned short f,d,o,h,w,x,y,oh,ow;
+  short imgPart[CONV2_DIM][CONV2_DIM];
+  short conv_px;
+  
+  for(f=0;f<CONV2_NBOUTPUT;f++){
+    for(d=0;d<POOL1_NBOUTPUT;d++){
+
+      for(h=0;h<CONV2_HEIGHT;h++){
+        for(w=0;w<CONV2_WIDTH;w++){
+          
+          for(y = 0; y < CONV2_DIM; y++){ //prepare 5X5 imgPart for the convolution
+            for(x = 0; x < CONV2_DIM; x++){
+              imgPart[y][x]=input[d][y+h][x+w];
+            }
+          }
+
+          conv_px=sumProduct(imgPart,kernel[f][d]);
+
+          if(d==0){ //to initialize first element
+            output[f][h][w] = conv_px;
+          }else{
+            output[f][h][w]+= conv_px;
+          }
+        }
+      } 
+    }
+
+    for(oh=0;oh<CONV2_HEIGHT;oh++){
+      for(ow=0;ow<CONV2_WIDTH;ow++){
+        //neuron activation
+        if(output[f][oh][ow]+bias[f]<=0){
+          output[f][oh][ow]=0;
+        }else{
+          output[f][oh][ow]=output[f][oh][ow] + bias[f];
+        }
+      }
+    }
+  }
+}
---
+++ FIXED_POINT_NO_HDF5/fc.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "lenet_cnn_float.h"
+
+/*void Softmax(short vector_in[FC2_NBOUTPUT], int vector_out[FC2_NBOUTPUT]){
+  long vector_exp[FC2_NBOUTPUT];
+  long exp_sum=0;
+  int vector_summy[FC2_NBOUTPUT];
+  short bckshifted_input;
+
+  for (short i = 0; i < FC2_NBOUTPUT; i++){
+    bckshifted_input=vector_in[i] >> FIXED_POINT;
+    vector_exp[i]=exp(bckshifted_input);
+    exp_sum+=vector_exp[i];
+  }
+  
+  exp_sum = exp_sum >> 4;
+  //printf("%lu\n",exp_sum);
+  for(short j = 0; j < FC2_NBOUTPUT; j++){
+    vector_out[j]=vector_exp[j]/exp_sum;
+    //vector_out[j]=0;
+  }
+  //printf("\n");
+}*/
+
+
+void Softmax(short vector_in[FC2_NBOUTPUT], float vector_out[FC2_NBOUTPUT]){
+  float vector_exp[FC2_NBOUTPUT];
+  float exp_sum=0;
+  short bckshifted_input;
+
+  for (short i = 0; i < FC2_NBOUTPUT; i++){
+    bckshifted_input=vector_in[i] >> FIXED_POINT;
+    vector_exp[i]=exp(bckshifted_input);
+    exp_sum+=vector_exp[i];
+  }
+
+  for(short j = 0; j < FC2_NBOUTPUT; j++){
+    vector_out[j]=vector_exp[j]/exp_sum;
+  }
+}
+
+
+void Fc1_40_400(    short 	input[POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH], 			        // IN
+			        short 	kernel[FC1_NBOUTPUT][POOL2_NBOUTPUT][POOL2_HEIGHT][POOL2_WIDTH],	// IN
+			        short 	bias[FC1_NBOUTPUT],							                        // IN
+			        short 	output[FC1_NBOUTPUT]) 							                    // OUT
+{
+  unsigned short o,d,h,w;
+  short fc_sum;
+  int temp_sum;
+
+  for(o = 0; o < FC1_NBOUTPUT; o++){
+    temp_sum=0;
+    for(d=0;d<POOL2_NBOUTPUT;d++){
+      for(h = 0; h < POOL2_HEIGHT; h++){
+        for(w = 0; w < POOL2_WIDTH; w++){
+          temp_sum+=(input[d][h][w]*kernel[o][d][h][w]);
+        }
+      }
+    }
+    fc_sum=temp_sum >> FIXED_POINT;
+    //neuron activation
+    if(fc_sum+bias[o]<=0){
+      output[o]=0;
+    }else{
+      output[o]=fc_sum+bias[o];
+    }
+  }
+
+}
+
+void Fc2_400_10(	short 	input[FC1_NBOUTPUT], 			        // IN
+			        short 	kernel[FC2_NBOUTPUT][FC1_NBOUTPUT],	    // IN
+			        short 	bias[FC2_NBOUTPUT],			            // IN
+			        short 	output[FC2_NBOUTPUT]) 			        // OUT
+{
+  unsigned short o,d;
+  int temp_sum;
+  short fc_sum;
+
+  for(o = 0; o < FC2_NBOUTPUT; o++){
+    temp_sum=0;
+    for(d=0;d<FC1_NBOUTPUT;d++){
+#pragma HLS PIPELINE
+      temp_sum+=(input[d]*kernel[o][d]);
+    }
+    fc_sum=temp_sum >> FIXED_POINT;
+    output[o]=fc_sum+bias[o];
+    
+    //neuron activation
+    /*if(fc_sum+bias[o]<=0){
+      output[o]=0;
+    }else{
+      output[o]=fc_sum+bias[o];
+    }*/
+  }  
+}
