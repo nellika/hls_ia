6932f66d18212a1b0c23d38162fcfdf8c4e62403
nellika <nynelli@yahoo.de>
2020-11-09 09:58:12 +0100

    reduce loops with function removal >> sumProduct

--- FIXED_POINT_NO_HDF5/conv.c
+++ FIXED_POINT_NO_HDF5/conv.c
@@ -3,6 +3,7 @@
 
 #include "lenet_cnn_float.h"
 
+/*
 short sumProduct( short imgPart[CONV1_DIM][CONV1_DIM], short filter[CONV1_DIM][CONV1_DIM]){
   unsigned short y,x;
   int conv_result=0;
@@ -13,7 +14,7 @@ short sumProduct( short imgPart[CONV1_DIM][CONV1_DIM], short filter[CONV1_DIM][C
       }
   }
   return conv_result >> FIXED_POINT;
-}
+}*/
 
 void Conv1_28x28x1_5x5x20_1_0(  unsigned char input[IMG_DEPTH][IMG_HEIGHT][IMG_WIDTH], 	                // IN [1][28][28]
 				                short kernel[CONV1_NBOUTPUT][IMG_DEPTH][CONV1_DIM][CONV1_DIM], 	// IN [20][1][5][5]
@@ -22,27 +23,29 @@ void Conv1_28x28x1_5x5x20_1_0(  unsigned char input[IMG_DEPTH][IMG_HEIGHT][IMG_W
 {
   unsigned short o,h,w,x,y;
   short imgPart[CONV1_DIM][CONV1_DIM];
-  short conv_px;
+  int conv_px_sum;
   
   for(o = 0; o < CONV1_NBOUTPUT; o++){
     for(h = 0; h < CONV1_HEIGHT; h++){
       for(w = 0; w < CONV1_WIDTH; w++){
         //prepare 5X5 imgPart for the convolution
+        conv_px_sum = 0;
+
         for(y = 0; y < CONV1_DIM; y++){
           for(x = 0; x < CONV1_DIM; x++){
-            imgPart[y][x]=input[0][h+y][w+x];
+            //imgPart[y][x]=input[0][h+y][w+x];
+            conv_px_sum = conv_px_sum + input[0][h+y][w+x]*kernel[o][0][y][x];
           }
         }
-        conv_px=sumProduct(imgPart,kernel[o][0]);
-        
+        //conv_px=sumProduct(imgPart,kernel[o][0]);
         //neuron activation
         //<=-100 works with better accuracy, but thats not the point here...
-        if(conv_px+bias[o]<=0){
+        if(conv_px_sum+bias[o]<=0){
           output[o][h][w]=0;
         }else{
-          output[o][h][w]=conv_px + bias[o];
+          conv_px_sum = conv_px_sum >> FIXED_POINT;
+          output[o][h][w]=conv_px_sum + bias[o];
         }
-        
       }
     }
   }
@@ -56,26 +59,28 @@ void Conv2_12x12x20_5x5x40_1_0( short input[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_
 {
   unsigned short f,d,o,h,w,x,y,oh,ow;
   short imgPart[CONV2_DIM][CONV2_DIM];
-  short conv_px;
+  int conv_px_sum;
   
   for(f=0;f<CONV2_NBOUTPUT;f++){
     for(d=0;d<POOL1_NBOUTPUT;d++){
 
       for(h=0;h<CONV2_HEIGHT;h++){
         for(w=0;w<CONV2_WIDTH;w++){
-          
+          conv_px_sum = 0;
           for(y = 0; y < CONV2_DIM; y++){ //prepare 5X5 imgPart for the convolution
             for(x = 0; x < CONV2_DIM; x++){
-              imgPart[y][x]=input[d][y+h][x+w];
+              //imgPart[y][x]=input[d][y+h][x+w];
+              conv_px_sum = conv_px_sum + input[d][h+y][w+x]*kernel[f][d][y][x];
             }
           }
 
-          conv_px=sumProduct(imgPart,kernel[f][d]);
+          //conv_px=sumProduct(imgPart,kernel[f][d]);
+          conv_px_sum = conv_px_sum >> FIXED_POINT;
 
           if(d==0){ //to initialize first element
-            output[f][h][w] = conv_px;
+            output[f][h][w] = conv_px_sum;
           }else{
-            output[f][h][w]+= conv_px;
+            output[f][h][w]+= conv_px_sum;
           }
         }
       } 
