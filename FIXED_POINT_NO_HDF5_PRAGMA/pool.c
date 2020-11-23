7ea7fbcc32805b3d9ba231bab058ae1ddba68acc
nellika <nynelli@yahoo.de>
2020-11-23 13:02:05 +0100

    removing maxPool function

--- FIXED_POINT_NO_HDF5_PRAGMA/pool.c
+++ FIXED_POINT_NO_HDF5_PRAGMA/pool.c
@@ -12,17 +12,17 @@
 
 #include "lenet_cnn_float.h"
 
-short maxPooling(short poolArray[]){
+/*short maxPooling(short poolArray[]){
     short max=poolArray[0];
     unsigned short p;
 
-    for (p = 1; p < POOL1_DIM*POOL1_DIM; p++){
+    for (p = 1; p < 4; p++){
         if(poolArray[p] > max){
           max = poolArray[p];
         }
     }
     return max;
-}
+}*/
 
 void Pool1_24x24x20_2x2x20_2_0( short input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_WIDTH],   // IN
                                 short output[POOL1_NBOUTPUT][POOL1_HEIGHT][POOL1_WIDTH])  // OUT
@@ -35,7 +35,12 @@ void Pool1_24x24x20_2x2x20_2_0( short input[CONV1_NBOUTPUT][CONV1_HEIGHT][CONV1_
       for (h = 0; h < CONV1_HEIGHT; h+=2){  // 20*12 > 240
         for (w = 0; w < CONV1_WIDTH; w+=2){ // 20*12*12 > 2880 iterations
             // select max from 2x2 matrix
-            maxPool=maxPooling((short []){input[i][h][w],input[i][h+1][w],input[i][h][w+1],input[i][h+1][w+1]});
+            //maxPool=maxPooling((short []){input[i][h][w],input[i][h+1][w],input[i][h][w+1],input[i][h+1][w+1]});
+            maxPool=input[i][h][w];
+            if(maxPool < input[i][h+1][w] ) maxPool = input[i][h+1][w];
+            if(maxPool < input[i][h][w+1] ) maxPool = input[i][h][w+1];
+            if(maxPool < input[i][h+1][w+1] ) maxPool = input[i][h+1][w+1];
+
             output[i][h>>1][w>>1]=maxPool;
         }
       }
@@ -53,7 +58,12 @@ void Pool2_8x8x40_2x2x40_2_0( short input[CONV2_NBOUTPUT][CONV2_HEIGHT][CONV2_WI
       for (h = 0; h < CONV2_HEIGHT; h+=2){  // 40*4 > 160
         for (w = 0; w < CONV2_WIDTH; w+=2){ // 40*4*4 > 640 iterations
             // select max from 2x2 matrix
-            maxPool=maxPooling((short []){input[j][h][w],input[j][h+1][w],input[j][h][w+1],input[j][h+1][w+1]});
+            //maxPool=maxPooling((short []){input[j][h][w],input[j][h+1][w],input[j][h][w+1],input[j][h+1][w+1]});
+            maxPool=input[j][h][w];
+            if(maxPool < input[j][h+1][w] ) maxPool = input[j][h+1][w];
+            if(maxPool < input[j][h][w+1] ) maxPool = input[j][h][w+1];
+            if(maxPool < input[j][h+1][w+1] ) maxPool = input[j][h+1][w+1];
+            
             output[j][h>>1][w>>1]=maxPool;
         }
       }
